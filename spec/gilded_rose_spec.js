da851eb59197e339396dcbe678ef40cd436f4ec5
Ravindra R. Jaju <jaju@msync.org>
2018-01-23 14:52:44 +0530

    Extracted 2 small methods for updating item quality, and tests for them. Also, locally-scoped current_item for each loop.

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,24 @@
+var assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
 var gilded_rose = require('../src/gilded_rose');
+var Item = gilded_rose.Item;
 var update_quality = gilded_rose.update_quality;
 
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
-    update_quality();
+  describe("Item", function() {
+    it("should devalue an item's quality by given value", function() {
+      var item = new Item("Some item", 10, 10);
+      gilded_rose.downgrade_item_quality(item, 1);
+      expect(item.quality).to.equal(9);
+    });
+    
+    it("should increase an item's quality by given value", function() {
+      var item = new Item("Some item", 10, 10);
+      gilded_rose.upgrade_item_quality(item, 10);
+      expect(item.quality).to.equal(20);
+    })
   });
-
+  
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,52 +13,64 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
+function downgrade_item_quality(item, value) {
+  item.quality = item.quality - value
+}
+
+function upgrade_item_quality(item, value) {
+  item.quality = item.quality + value;
+}
+
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+    var current_item = items[i];
+    if (current_item.name != 'Aged Brie' && current_item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (current_item.quality > 0) {
+        if (current_item.name != 'Sulfuras, Hand of Ragnaros') {
+          downgrade_item_quality(current_item, 1);
         }
       }
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+      if (current_item.quality < 50) {
+        current_item.quality = current_item.quality + 1
+        if (current_item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (current_item.sell_in < 11) {
+            if (current_item.quality < 50) {
+              upgrade_item_quality(current_item, 1);
             }
           }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+          if (current_item.sell_in < 6) {
+            if (current_item.quality < 50) {
+              upgrade_item_quality(current_item, 1);
             }
           }
         }
       }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+    if (current_item.name != 'Sulfuras, Hand of Ragnaros') {
+      current_item.sell_in = current_item.sell_in - 1;
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+    if (current_item.sell_in < 0) {
+      if (current_item.name != 'Aged Brie') {
+        if (current_item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (current_item.quality > 0) {
+            if (current_item.name != 'Sulfuras, Hand of Ragnaros') {
+              downgrade_item_quality(current_item, 1);
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          current_item.quality = current_item.quality - current_item.quality
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (current_item.quality < 50) {
+          upgrade_item_quality(current_item, 1);
         }
       }
     }
   }
 }
 
+module.exports.Item = Item;
+module.exports.downgrade_item_quality = downgrade_item_quality;
+module.exports.upgrade_item_quality = upgrade_item_quality;
 module.exports.update_quality = update_quality;
